5687123424eca6abe83e96ba2320070959602a0b
darshanyn04 <darshanynvfx@gmail.com>
2025-10-28 11:49:48 +0530

    Update package.json for npm publish

--- index.js
+++ index.js
@@ -1,29 +1,81 @@
+// import { execSync } from "child_process";
+
+// /**
+//  * Open a browser with the given URL
+//  */
+// export function open(browser, url) {
+//   if (!browser) throw new Error("Browser name required");
+//   if (!url) throw new Error("URL required");
+
+//   let script = "";
+
+//   switch (browser.toLowerCase()) {
+//     case "safari":
+//       script = `osascript -e 'tell application "Safari" to open location "${url}"'`;
+//       break;
+//     case "chrome":
+//       script = `osascript -e 'tell application "Google Chrome" to open location "${url}"'`;
+//       break;
+//     case "firefox":
+//       script = `open -a "Firefox" "${url}"`;
+//       break;
+//     default:
+//       throw new Error(`Unsupported browser: ${browser}`);
+//   }
+
+//   execSync(script);
+// }
+
+// /**
+//  * Close a browser completely
+//  */
+// export function close(browser) {
+//   if (!browser) throw new Error("Browser name required");
+
+//   let script = "";
+
+//   switch (browser.toLowerCase()) {
+//     case "safari":
+//       script = `osascript -e 'tell application "Safari" to quit'`;
+//       break;
+//     case "chrome":
+//       script = `osascript -e 'tell application "Google Chrome" to quit'`;
+//       break;
+//     case "firefox":
+//       script = `osascript -e 'tell application "Firefox" to quit'`;
+//       break;
+//     default:
+//       throw new Error(`Unsupported browser: ${browser}`);
+//   }
+
+//   execSync(script);
+// }
 import { execSync } from "child_process";
 
 /**
- * Open a browser with the given URL
+ * Open a browser with the given URL (visible window)
  */
 export function open(browser, url) {
   if (!browser) throw new Error("Browser name required");
   if (!url) throw new Error("URL required");
 
-  let script = "";
+  let command = "";
 
   switch (browser.toLowerCase()) {
     case "safari":
-      script = `osascript -e 'tell application "Safari" to open location "${url}"'`;
+      command = `open -a "Safari" "${url}"`;
       break;
     case "chrome":
-      script = `osascript -e 'tell application "Google Chrome" to open location "${url}"'`;
+      command = `open -a "Google Chrome" "${url}"`;
       break;
     case "firefox":
-      script = `open -a "Firefox" "${url}"`;
+      command = `open -a "Firefox" "${url}"`;
       break;
     default:
       throw new Error(`Unsupported browser: ${browser}`);
   }
 
-  execSync(script);
+  execSync(command);
 }
 
 /**
